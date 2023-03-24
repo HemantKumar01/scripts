c378c0e051629f89f9f04ee88093f9638f7f4429
HemantKumar01 <ytbhemant@gmail.com>
2023-03-24 17:17:15 +0530

    first commit

---
+++ allen_SaveQuestion.js
@@ -0,0 +1,29 @@
+//!-------------------COPY BELOW TO CONSOLE-------------------
+//after running this script in console on allen test solution website,click each question, then copy console(or save it) and then remove all "VM..." type extra stuff
+var $0 = document.querySelector(".questioninr");
+var inh = $0.innerHTML;
+var timerINH = setInterval(() => {
+  if ($0.innerHTML == inh) {
+    return;
+  }
+  console.log($0.innerHTML);
+  inh = $0.innerHTML;
+}, 10); //check every 10ms
+
+//! ------------------------BELOW TO BE USED ON HTML SAVED FROM ABOVE------------------
+var questions = document.querySelectorAll("[ng-bind-html='questionText']");
+var options = document.querySelectorAll(".optncntnt");
+function printQuesAns() {
+  var obj = {};
+  for (var i = 0; i < questions.length; i++) {
+    obj[i + 1] = {
+      q: questions[i].innerHTML,
+      o1: options[4 * i]?.innerHTML || "",
+      o2: options[4 * i + 1]?.innerHTML || "",
+      o3: options[4 * i + 2]?.innerHTML || "",
+      o4: options[4 * i + 3]?.innerHTML || "",
+      ans: "",
+    };
+  }
+  console.log(obj);
+}
